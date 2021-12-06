045fb88d9c0c8f19d18c79c18bd0ac6aa2935c17
Derrick Brown <ricochetbrown@gmail.com>
2021-12-06 07:32:42 -0500

    Try to get uncovered code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stats-frontend';
+
+  didNotHitThis() {
+    console.log('This should be uncovered code.');
+  }
 }
